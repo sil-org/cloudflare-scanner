98aeb30726d5c7cd666f5147b6435b100bf30f26
Unknown <baggerone@gmail.com>
2019-07-11 16:13:04 -0400

    initial commit

---
+++ codeship/deploy-prod.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Exit script with error if any step fails.
+set -e
+
+# Build binaries
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/build.sh
+
+# Export env vars
+export AWS_REGION="${AWS_REGION}"
+export CF_API_EMAIL="${CF_API_EMAIL}"
+export CF_API_KEY="${CF_API_KEY}"
+export CF_CONTAINS_STRING="${CF_CONTAINS_STRING}"
+export CF_ZONE_NAMES="${CF_ZONE_NAMES}"
+export SES_SUBJECT="${SES_SUBJECT}"
+export SES_RETURN_TO_ADDR="${SES_RETURN_TO_ADDR}"
+export SES_RECIPIENT_EMAILS="${SES_RECIPIENT_EMAILS}"
+
+serverless deploy -v --stage prod
